3b07477abe9dd03b3790c8fda979a32565881f36
Davi Lucciola <davilucciola14@gmail.com>
2024-01-22 03:14:30 -0300

    feat: initializing hero

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect('/home') 
-
-@app.route('/home')
-def home():
     return render_template('index.html')
 
-@app.route('/sobre-mim')
-def about():
-    return render_template('sobre-mim.html')
+@app.route('/contato')
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
