5d446b940e506a4b41a0069e886210fb2e5580e1
Davi Lucciola <davilucciola14@gmail.com>
2024-02-03 19:07:45 -0300

    feat: formulário de contato implementado

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/contato')
+@app.route('/contato', methods=['GET'])
 def contact():
     return render_template('contact.html')
+
+@app.route('/contato', methods=['POST'])
+def contact_message():
+    remetente = request.form.get('remetente')
+    assunto = request.form.get('assunto')
+    mensagem = request.form.get('mensagem')
+
+    resposta = f'Obrigado {remetente} por enviar uma mensagem para Davi Lucciola com o assunto {assunto}'
+    return render_template('contact.html', mensagem=resposta)
 
 
 if __name__ == '__main__':
